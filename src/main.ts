b25a9551ae4b57192be8a7ee0e7eac0a2458df5e
wangchuang <wangchuangweb@163.com>
2022-01-24 21:39:24 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+debugger
+
+createApp(App).mount("#app");
